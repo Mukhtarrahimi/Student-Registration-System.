e61b29ce6a27035fabba3f1e16ac8e7966a64d23
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-14 07:56:00 +0430

    Add top frames.

--- main.py
+++ main.py
@@ -39,5 +39,9 @@ else:
     
     file.save("student_data.xlsx")
 
+# top frames
+Label(root, text= "Email: Mukhtarrahimi110@gmail.com", width=10, height=3, bg="#f0687c", anchor='e').pack(side=TOP, fill=X)
+Label(root, text= "STUDENT REGISTRATION", width=10, height=2, bg="#c36464",fg="#ffffff", font="arial 20 bold").pack(side=TOP, fill=X)
+
 
 root.mainloop()
