67e91c5b316094da321785f2c45d3bc14a714b83
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-14 07:38:42 +0430

    Add initialize tkinter.

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ from openpyxl import Workbook
 import pathlib
 
 background = "#06283d"
+
+root = Tk()
+root.title("Student Registration System")
+root.geometry("1250x700+210+100")
+root.config()
+
+
+root.mainloop()
