076c51af979a5b8765dd453168e34740a7525b26
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-14 07:48:40 +0430

    Initialize student_data.xlsx with header columns.

--- main.py
+++ main.py
@@ -13,11 +13,31 @@ background = "#06283d"
 framebg = "#ededed"
 framefg = "#06283d"
 
-
 root = Tk()
 root.title("Student Registration System")
 root.geometry("1250x700+210+100")
 root.config(bg=background)
+
+file = pathlib.Path("student_data.xlsx")
+if file.exists():
+    pass
+else:
+    file = Workbook()
+    sheet = file.active
+    sheet['A1']="Registration No."
+    sheet['B1']="Name"
+    sheet['C1']="Class"
+    sheet['D1']="Gender"
+    sheet['E1']="DOB"
+    sheet['F1']="Date Of Registration"
+    sheet['G1']="Religion"
+    sheet['H1']="Skill"
+    sheet['I1']="Father Name"
+    sheet['J1']="Mother Name"
+    sheet['K1']="Father's Occupation"
+    sheet['L1']="Mother's Occupation"
+    
+    file.save("student_data.xlsx")
 
 
 root.mainloop()
