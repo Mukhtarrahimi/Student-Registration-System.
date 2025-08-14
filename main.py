d106cebdeac7596b9e4d42f343cc79a29084f605
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-14 08:12:29 +0430

    Add registration & date.

--- main.py
+++ main.py
@@ -54,9 +54,15 @@ imageicon1 = PhotoImage(file="images/Layer 4.png")
 update_button = Button(root, image=imageicon1, bg="#c36464")
 update_button.place(x=110, y=64)
 
+# Registration and date
+Label(root, text="Registration No:", bg=framebg, fg=framefg, font="arial 13").place(x=30, y=150)
+Label(root, text="ِDate:", bg=framebg, fg=framefg, font="arial 13").place(x=500, y=150)
 
+Registration = StringVar()
+Date = StringVar()
 
-
+reg_entry = Entry(root, textvariable = Registration, width=15, font="arial 10")
+reg_entry.place(x=160, y=150)
 
 
 root.mainloop()
