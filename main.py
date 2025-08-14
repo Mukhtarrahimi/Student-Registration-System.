11e61918bc64ae76f3c35b2c011983845b1df07e
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-14 07:36:13 +0430

    Add os, poenpyxl, pathlib, pil.

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from datetime import date
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter.ttk import Combobox
+from PIL import Image, ImageTk
+import os
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
